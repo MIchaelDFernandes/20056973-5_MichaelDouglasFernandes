6b78079ec0eb63c1a05a1cb953e185cc7ce70431
MIchaelDFernandes <mdouglasfernandes@gmail.com>
2021-08-14 15:53:24 -0300

    criar dados.php

---
+++ dados/dados.php
@@ -0,0 +1,16 @@
+<?php
+
+$bolos = [
+    ["Laranja","images/laranja.jpg", "Leite integral, farinha de trigo enriquecida com ferro e
+        ácido fólico, açúcar, ovo, gordura vegetal, laranja, fermentos químicos pirofosfato de sódio, 
+        bicarbonato de sódio e fosfato monocálcico. CONTÉM GLÚTEN E LACTOSE. Alérgicos: contém leite, derivados 
+        do trigo e ovo."],
+    ["Formigueiro","images/formigueiro.jpg", "Leite integral, farinha de trigo enriquecida com ferro e 
+         ácido fólico, açúcar, ovo, gordura vegetal, chocolate granulado, fermentos químicos pirofosfato de sódio, 
+        bicarbonato de sódio e fosfato monocálcico. CONTÉM GLÚTEN E LACTOSE. Alérgicos: contém leite e derivados, 
+        derivados do trigo e ovo."],
+    ["Ameixa","images/ameixa.jpg","Leite integral, farinha de trigo enriquecida com ferro e ácido fólico, açúcar, "
+        . "ovo, gordura vegetal, ameixa sem caroço, fermentos químicos pirofosfato de sódio, bicarbonato de sódio e fosfato "
+        . "monocálcico. CONTÉM GLÚTEN E LACTOSE. Alérgicos: contém leite e derivados, derivados do trigo e ovo."],
+];
+
