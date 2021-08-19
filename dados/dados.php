956e55cc969ae8a3555f807a2a87e0d347c3d2cd
MIchaelDFernandes <mdouglasfernandes@gmail.com>
2021-08-19 19:49:38 -0300

    adicionar imagens dos bolos e paginas em branco

--- dados/dados.php
+++ dados/dados.php
@@ -1,16 +1,22 @@
 <?php
 
 $bolos = [
-    ["Laranja","images/laranja.jpg", "Leite integral, farinha de trigo enriquecida com ferro e
-        ácido fólico, açúcar, ovo, gordura vegetal, laranja, fermentos químicos pirofosfato de sódio, 
-        bicarbonato de sódio e fosfato monocálcico. CONTÉM GLÚTEN E LACTOSE. Alérgicos: contém leite, derivados 
-        do trigo e ovo."],
-    ["Formigueiro","images/formigueiro.jpg", "Leite integral, farinha de trigo enriquecida com ferro e 
-         ácido fólico, açúcar, ovo, gordura vegetal, chocolate granulado, fermentos químicos pirofosfato de sódio, 
-        bicarbonato de sódio e fosfato monocálcico. CONTÉM GLÚTEN E LACTOSE. Alérgicos: contém leite e derivados, 
-        derivados do trigo e ovo."],
-    ["Ameixa","images/ameixa.jpg","Leite integral, farinha de trigo enriquecida com ferro e ácido fólico, açúcar, "
-        . "ovo, gordura vegetal, ameixa sem caroço, fermentos químicos pirofosfato de sódio, bicarbonato de sódio e fosfato "
-        . "monocálcico. CONTÉM GLÚTEN E LACTOSE. Alérgicos: contém leite e derivados, derivados do trigo e ovo."],
+    ["Laranja","images/laranja.jpg"],
+    ["Formigueiro","images/formigueiro.jpg"],
+    ["Ameixa","images/ameixa.jpg"],
 ];
 
+
+$descricoes = ["Laranja" => "Leite integral, farinha de trigo enriquecida com ferro e
+        ácido fólico, açúcar, ovo, gordura vegetal, laranja, fermentos químicos pirofosfato de sódio, 
+        bicarbonato de sódio e fosfato monocálcico. CONTÉM GLÚTEN E LACTOSE. Alérgicos: contém leite, derivados 
+        do trigo e ovo.", 
+    "Formigueiro" => "Leite integral, farinha de trigo enriquecida com ferro e 
+         ácido fólico, açúcar, ovo, gordura vegetal, chocolate granulado, fermentos químicos pirofosfato de sódio, 
+        bicarbonato de sódio e fosfato monocálcico. CONTÉM GLÚTEN E LACTOSE. Alérgicos: contém leite e derivados, 
+        derivados do trigo e ovo.",
+    "Ameixa" => "Leite integral, farinha de trigo enriquecida com ferro e ácido fólico, açúcar, "
+        . "ovo, gordura vegetal, ameixa sem caroço, fermentos químicos pirofosfato de sódio, bicarbonato de sódio e fosfato "
+        . "monocálcico. CONTÉM GLÚTEN E LACTOSE. Alérgicos: contém leite e derivados, derivados do trigo e ovo."
+    
+    ];
--- index.php
+++ index.php
@@ -1,33 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>Starter Template for Bootstrap</title>
+        <title>Bolos Caseiros</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
         <header class="container-fluid">
             <nav class="bg-danger fixed-top navbar navbar-dark navbar-expand-md">
-                <a class="navbar-brand" href="#">Bolos Caseiros</a>
+                <a class="navbar-brand" href="index.php">Bolos Caseiros</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarsExampleDefault">
                     <ul class="mr-auto navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Quem Somos</a>
+                            <a class="nav-link" href="index.php?acessando=quemsomos">Quem Somos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Contato</a>
+                            <a class="nav-link" href="index.php?acessando=contato">Contato</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Localização</a>
-                        </li>
+                        
                     </ul>
                 </div>
             </nav>
@@ -36,7 +34,18 @@
                 <p>Trabalhamos com encomendas desde 2005!</p>
             </div>
         </header>
-        <main class="container-fluid"></main>
+        <main class="container-fluid">
+        <?php
+            $pagina = isset( $_GET['acessando'] ) ? $_GET['acessando'] : '';
+            if($pagina==''){
+                include ('home.php');}		
+            elseif(file_exists($pagina.'.php')){
+                include ($pagina.'.php');
+            }
+            else{ 
+                include ('home.php');}
+        ?>
+        </main>
         <footer class="bg-success container-fluid">
             <div class="row text-center">
                 <div class="col">
@@ -44,10 +53,10 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-6 col-sm-12 text-right"> 
-                    <p> Desenvolvido por: 
+                <div class="border border-0 border-light col-md-6 col-sm-12 font-weight-bold text-center"> 
+                    <p> Desenvolvido por:   
                 </div>
-                <div class="col-md-6 col-sm-12"> 
+                <div class="border-bottom-0 border-left-0 border-light border-top-0 col-md-6 col-sm-12"> 
                     <p> Michael Douglas Fernandes</p>
                     <p>R.A.:&nbsp;20056973-5</p>
                     <p>para curso de Sistemas para Internet na Unicesumar</p>
